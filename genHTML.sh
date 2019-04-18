53a44c41b58a7fdd08b9d5bca222c28ab30a69fc
Your Name <you@example.com>
2019-04-18 21:33:41 +0300

    added genHTML.sh

---
+++ genHTML.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# replace ip in /etc/hosts for domain rybalkatut.by
+
+#run
+wget -r -l 7 -p -nc -E http://rybalkatut.by/
+find . -type f -print0 | xargs -0 sed -i 's/http:\/\/rybalka/https:\/\/rybalka/g'
+git commit -a -m "meow"
+git push origin master
