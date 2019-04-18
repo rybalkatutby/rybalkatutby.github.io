1ce04a643d8eff564270f83dbf73f86354ae7914
Your Name <you@example.com>
2019-04-18 22:05:39 +0300

    copyright fix and gallery

--- genHTML.sh
+++ genHTML.sh
@@ -4,6 +4,7 @@
 
 #run
 wget -r -l 7 -p -nc -E http://rybalkatut.by/
+cd rybalkatut.by
 find . -type f -print0 | xargs -0 sed -i 's/http:\/\/rybalka/https:\/\/rybalka/g'
 git commit -a -m "meow"
 git push origin master
